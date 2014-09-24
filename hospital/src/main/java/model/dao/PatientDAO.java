a7d0e6ff3e75d726dfb47c546367a025c884e42d
kwgoon <kwgoon@gmail.com>
2014-09-25 01:27:55 +0900

    a

--- hospital/src/main/java/model/dao/PatientDAO.java
+++ hospital/src/main/java/model/dao/PatientDAO.java
@@ -1,5 +1,19 @@
 package model.dao;
 
+import model.dto.PatientDTO;
+
+import org.apache.ibatis.session.SqlSession;
+
+import util.DBUtil;
+
 public class PatientDAO {
 
+	static public void insertPatient(PatientDTO pt){
+		SqlSession session = DBUtil.getSqlSession(true);
+		try {
+			session.insert("Patient.insertInfo", pt);
+		} finally {
+			session.close();
+		}
+	}
 }
